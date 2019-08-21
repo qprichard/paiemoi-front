c46bbf9a469356ee774f00b6e44e50c6b4a11fe2
quentin <quentinrichard97@gmail.com>
2019-08-21 04:37:02 +0200

    add connect reducer

---
+++ src/api/connect/state/index.js
@@ -0,0 +1,9 @@
+export {
+  getAuthorizationHeader,
+  logout,
+  login,
+  isLogged,
+  isPending,
+  getConnectedUser,
+  updateTokenFromLocalStorage } from "./actions";
+export { default as connectReducer} from "./reducer";
