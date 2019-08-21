8fcd354144803d4623ba988ae737eabfbc93fda1
quentin <quentinrichard97@gmail.com>
2019-08-21 04:38:59 +0200

    update pages

--- src/pages/user/connect.jsx
+++ src/pages/user/connect.jsx
@@ -1,11 +1,58 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Segment from "pages/common/blocks/segment";
+import { login, isLogged } from "api/connect/state";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { Input, Button, Grid, Form } from "semantic-ui-react";
 
-const Connect = () => {
+const Connect = ({ logged, connect, history }) => {
+
+  React.useEffect(() => {
+    if(logged) {
+      history.push('/home');
+    }
+  }, [logged, history]);
+
+  const [formValues, setFormValues] = React.useState({});
+
+  const handleConnect = React.useCallback(() => connect(formValues), [formValues, connect]);
+
   return (
-    <div>
-      Page de connexion..
-    </div>
+    <Form>
+      <Grid centered>
+        <Grid.Row>
+          <Grid.Column computer="8" mobile="16">
+            <Segment title="Connexion" center>
+              <Form.Field>
+                <label>Email</label>
+                <Input placeholder="Email..." onChange={(_,{value}) => setFormValues({...formValues, email: value})}/>
+              </Form.Field>
+              <Form.Field>
+                <label>Mot de passe</label>
+                <Input type="password" placeholder="Mot de pass..." onChange={(_,{value}) => setFormValues({...formValues, password: value})}/>
+              </Form.Field>
+              <Button primary onClick={ handleConnect }>Se connecter</Button>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Form>
   )
 }
 
-export default Connect;
+Connect.propTypes = {
+  logged: PropTypes.bool.isRequired,
+  connect: PropTypes.func.isRequired,
+  history: PropTypes.any.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+  logged: isLogged(state),
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  connect: (values) => dispatch(login(values))
+})
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Connect));
--- src/router.jsx
+++ src/router.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import{ Switch, Route, Redirect } from "react-router-dom";
+import AuthController from "./authController";
+import{ Switch, Route } from "react-router-dom";
 import Home from "pages/home/container";
 import Connect from "pages/user/connect";
 import CreateUser from "pages/user/create";
 
 const AppRouter = () => {
   return (
-    <Switch>
-      <Route path="/home" component={ Home }/>
-
-      <Route path="/connect" component={ Connect }/>
-      <Route path="/subscribe" component={ CreateUser }/>
-
-      <Redirect to="/home"/>
-    </Switch>
+    <AuthController>
+      <Switch>
+          <Route path="/home" component={ Home }/>
+          <Route path="/connect" component={ Connect }/>
+          <Route path="/subscribe" component={ CreateUser }/>
+      </Switch>
+    </AuthController>
   )
 }
 
